d2a51bba73745d9105001c72d7cf3c9fbd9f88b5
Thiago Campos <commit@thigcampos.com>
2025-01-14 18:16:16 -0300

    include basic commands: add, remove and sync

---
+++ src/add.py
@@ -0,0 +1,23 @@
+IGNORED_FILES_AND_FOLDERS = ["man"]
+
+
+def handle_response(name: str, response: json) -> json:
+    """It loops through the response and get the necessary information"""
+    docset = {}
+    docset["name"] = name
+
+    for doc in response:
+        doc_type = doc["type"]
+        if doc_type == "dir":
+            docset["dir"] = doc["name"]
+        else:
+            docset["doc"] = doc["name"]
+            docset["path"] = doc["path"]
+            docset["sha"] = doc["sha"]
+            docset["download_url"] = doc["download_url"]
+
+        doc_name = doc["name"]
+        doc_path = doc["path"]
+        doc_sha = doc["sha"]
+        doc_download_url = doc["download_url"]
+    return response
--- src/main.py
+++ src/main.py
@@ -1,5 +1,54 @@
-def main:
+import os
+
+import requests
+from cyclopts import App
+
+GITHUB_TOKEN = os.environ["GITHUB_TOKEN"]
+IGNORED_FILES_AND_FOLDERS = ["man", "releases", "_", ".", "make", "bat"]
+
+app = App()
+
+
+def fetch_docs(docset_url: str):
+    print("Fetching docs from GitHub")
+    r = requests.get(docset_url)
+    return r
+
+
+def filter_files_and_folders(docs):
+    accepted_docs = []
+    for doc in docs:
+        if not any(st in doc["name"].lower() for st in IGNORED_FILES_AND_FOLDERS):
+            accepted_docs.append(doc)
+    return accepted_docs
+
+
+@app.command()
+def add(docset_url: str) -> None:
+    """Add a new docset to the database"""
+    response = fetch_docs(docset_url)
+    content = response.json()
+    valid_docs = filter_files_and_folders(content)
+    for doc in valid_docs:
+        print(doc["name"], doc["type"])
+
+
+@app.command()
+def remove(docset_name: str) -> None:
+    """Remove a docset from the database"""
+    pass
+
+
+@app.command()
+def sync():
+    """Sync all docsets with remote"""
+    pass
+
+
+@app.default()
+def main():
     print("Hello, World!")
 
+
 if __name__ == "__main__":
-    main()
+    app()
